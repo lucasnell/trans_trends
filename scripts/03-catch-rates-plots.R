a2a4d4517b99eddfec1cac82179ccab57528a2c7
Lucas Nell <lucnell@gmail.com>
2025-06-09 12:34:45 -0400

    Updated catch rates plots

--- scripts/03-catch-rates-plots.R
+++ scripts/03-catch-rates-plots.R
@@ -13,9 +13,27 @@ source("scripts/00-preamble.R")
 
 data_df <- read_rds(data_rds)
 
-type_pal <- c("black", viridis(100)[80])
+type_pal <- c("black", "goldenrod2")
+
+# Mean and standard deviation of log-transformed midge catch rate:
+midges_mu <- with(read_rds(data_rds), mean(log(midges / midge_days)))
+midges_sd <- with(read_rds(data_rds), sd(log(midges / midge_days)))
 
 
+
+
+with_options(pillar.sigfig = 7,
+             data_df |>
+                 filter(taxon_pretty == "Ground spiders") |>
+                 select(taxon_pretty, year, midges, midge_days) |>
+                 group_by(taxon_pretty, year) |>
+                 summarize(midges = mean(midges / midge_days), .groups = "drop"))
+with_options(pillar.sigfig = 7,
+             data_df |>
+                 filter(taxon_pretty == "Ground spiders") |>
+                 select(taxon_pretty, dist, midges, midge_days) |>
+                 group_by(taxon_pretty, dist) |>
+                 summarize(midges = mean(midges / midge_days), .groups = "drop"))
 
 
 
@@ -48,6 +66,7 @@ make_summ_df <- function(grp_var, taxa = NA, .trans_fun = NA) {
 
 
 
+
 with_options(pillar.sigfig = 7,
              make_summ_df(year) |>
                  filter(type == "Midge abundance") |>
@@ -76,47 +95,8 @@ with_options(pillar.sigfig = 7,
 # 6 gnap  Ground spiders 1.130506
 
 
-
-
-# # by time
-# # time_p <-
-# data_df |>
-#     ggplot(aes(year, y_noz))+
-#     facet_wrap(~taxon_pretty, nrow = 4,
-#                # scales = "free_y")+
-#                scales = "fixed")+
-#     geom_hline(yintercept = 0, color = "gray50")+
-#     # geom_line(aes(group = plot), linewidth = 0.2, color = "gray70") +
-#     geom_line(aes(group = plot), linewidth = 0.2, color = "gray30", alpha = 0.25) +
-#     geom_point(size = 1, alpha = 0.1, shape = 19, color = "gray30") +
-#     # geom_point(data = time_summ_df, aes(color = type), size = 2, shape = 1)+
-#     # geom_line(data = time_summ_df,
-#     geom_line(data = make_summ_df(year, .trans_fun = NA) |> filter(grepl("^Mean", type)),
-#               aes(color = type), linewidth = 0.75) +
-#     scale_color_manual(NULL, values = c("black", type_pal[1:2])) +
-#     scale_x_continuous("Year", breaks = c(2008,2012,2016),
-#                        limits = c(2007,2020)) +
-#     coord_cartesian(ylim = c(0, 1.130)) +
-#     scale_y_continuous(expression("Catch rate (days"*{}^{-1}*")"),
-#                        # limits = c(0, 2.6),
-#                        # breaks = log1p(c(0, 2, 8)),
-#                        # labels = c(0, 2, 8),
-#                        sec.axis = sec_axis(inv_trans_fun2,
-#                                            "Midge abundance")) +  # ,
-#                                            # breaks = log1p(20 * 40^(0:2)),
-#                                            # labels = c("20", "8e2", "32e3"))) +
-#     theme(legend.position = "top",
-#           axis.text.y.right = element_text(color = type_pal[3]),
-#           axis.ticks.y.right = element_line(color = type_pal[3]),
-#           # axis.title.y.right = element_text(color = type_pal[3])
-#           axis.title.y.right = element_blank()
-#           # axis.text.y.right = element_blank(),
-#           # axis.title.y.right = element_text(color = type_pal[3])
-#           )
-#
-
-
 cr_plotter <- function(.df, x_var, grp_var) {
+
     x_str <- as_name(quo(expr = {{x_var}}))
     txn <- .df$taxon_pretty |> paste() |> unique()
     stopifnot(length(txn) == 1)
@@ -131,9 +111,16 @@ cr_plotter <- function(.df, x_var, grp_var) {
     y_max <- c(2.569, 1.684, 1.329)[[y_idx]]
     y_labs <- list(c(0, 2, 8), c(0, 1, 4), c(0, 1, 2.5))[[y_idx]]
     # Similar but for transforming between catch rates and midge abundances:
-    b0 <- y_max / 2.45 * 0.8
-    trans_fun <- function(x) (x + 1.65) * b0
-    inv_trans_fun <- function(y) y / b0 - 1.65
+    b0 <- -midges_mu / midges_sd
+    # b1 <- y_max / 2.45 * 0.8
+    b1 <- y_max / 3.4
+    trans_fun <- function(x) (x - b0) * b1
+    inv_trans_fun <- function(y) {
+        cr_lz <- y / b1 + b0 # to z_trans(log(midges / midge_days))
+        cr_l <- cr_lz * midges_sd + midges_mu # to log(midges / midge_days)
+        cr <- exp(cr_l) # to midges / midge_days
+        return(cr)
+    }
     x_scale <- NULL
     if (x_str == "year") {
         x_scale <- scale_x_continuous("Year", breaks = 2010 + 4 * 0:2,
@@ -143,7 +130,7 @@ cr_plotter <- function(.df, x_var, grp_var) {
                                       breaks = log(c(5, 50, 150, 500)),
                                       labels = c(5, 50, 150, 500))
     }
-    .df |>
+    p <- .df |>
         ggplot(aes({{ x_var }}, y_noz))+
         facet_wrap(~taxon_pretty, nrow = 4, scales = "free_y")+
         geom_hline(yintercept = 0, color = "gray50")+
@@ -159,52 +146,57 @@ cr_plotter <- function(.df, x_var, grp_var) {
                            breaks = log1p(y_labs),
                            labels = y_labs,
                            sec.axis = sec_axis(inv_trans_fun,
-                                               "SD change in midge abundance",
-                                               breaks = -1:1)) +
+                                               expression("Midge catch rate (days"*{}^{-1}*")"),
+                                               breaks = 10^(0:2))) +
         theme(axis.title.y.right = element_text(color = type_pal[2]),
               axis.text.y.right = element_text(color = type_pal[2]),
-              axis.ticks.y.right = element_line(color = type_pal[2]))
+              axis.ticks.y.right = element_line(color = type_pal[2]),
+              plot.margin = margin(r=3, l=3, t=-9, b=3))
+    if (txn %in% levels(data_df$taxon_pretty)[c(1,3,5)]) {
+        p <- p + theme(axis.text.y.right = element_blank())
+    }
+    return(p)
 }
 
 
 
 
-time_plots <- data_df |>
+time_p <- data_df |>
     split(~ taxon_pretty) |>
-    map(cr_plotter, x_var = year, grp_var = plot)
+    map(cr_plotter, x_var = year, grp_var = plot) |>
+    do.call(what = wrap_plots) +
+    plot_layout(ncol = 2, axis_titles = "collect") &
+    theme(axis.title.y.right = element_blank(),
+          plot.tag.position = c(0.15, 1))
 
-
-# do.call(wrap_plots, time_plots) +
-#     plot_layout(ncol = 2, guides = "collect", axis_titles = "collect") +
-#     plot_annotation(title = "a", theme = theme(plot.title = element_text(
-#         face = "bold", size = 14))) &
-#     theme(legend.position = "top")
-
-
-dist_plots <- data_df |>
+dist_p <- data_df |>
     mutate(id = interaction(trans, year)) |>
     split(~ taxon_pretty) |>
-    map(cr_plotter, x_var = dist, grp_var = id)
+    map(cr_plotter, x_var = dist, grp_var = id) |>
+    do.call(what = wrap_plots) +
+    plot_layout(ncol = 2, axis_titles = "collect") &
+    theme(axis.title.y.left = element_blank(),
+          plot.tag.position = c(0, 1))
 
+# time_p +
+#     plot_layout(guides = "collect") &
+#     theme(legend.position = "top")
 
-# do.call(wrap_plots, dist_plots) +
-#     plot_layout(ncol = 2, guides = "collect", axis_titles = "collect") +
-#     plot_annotation(title = "b", theme = theme(plot.title = element_text(
-#         face = "bold", size = 14))) &
+# dist_p +
+#     plot_layout(guides = "collect") &
 #     theme(legend.position = "top")
 
 
-time_p <- do.call(wrap_plots, time_plots) +
-    plot_layout(ncol = 2, axis_titles = "collect") &
-    theme(axis.title.y.right = element_blank())
-dist_p <- do.call(wrap_plots, dist_plots) +
-    plot_layout(ncol = 2, axis_titles = "collect") &
-    theme(axis.title.y.left = element_blank())
-
-obs_data_p <- guide_area() / (time_p | dist_p) +
-    plot_layout(nrow = 2, guides = "collect", heights = unit(c(1, 1), c("cm", "null"))) +
+obs_data_p <- guide_area() + time_p + dist_p +
+    plot_layout(guides = "collect", heights = unit(c(1, 1), c("cm", "null")),
+                widths = c(1, 0.05, 1),
+                design = "AAA\nB#C") +
     plot_annotation(tag_levels = list(c("a", rep("", 5), "b", rep("", 5)))) &
     theme(legend.position = "top")
+
+obs_data_p
+
+
 
 
 # save_plot("catch-rates-year-dist", obs_data_p, w = 8, h = 5)
